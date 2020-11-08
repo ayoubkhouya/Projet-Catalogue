72aad5a825e865ba9b60ec95c3c484e55ac730da
ayoubkhouya <a.khouya.ing@gmail.com>
2020-11-08 17:59:48 +0100

    servlet catalogue

---
+++ CatalogueWebJEE/src/fr/cataloge/servlets/CategoriesServlet.java
@@ -0,0 +1,28 @@
+package fr.cataloge.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/categorie")
+public class CategoriesServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 4214547814660559772L;
+
+    public CategoriesServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/pages/categorie.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
