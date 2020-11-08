69e455d95bf0c904cf2277766e112eeef7869b70
ayoubkhouya <a.khouya.ing@gmail.com>
2020-11-08 18:52:57 +0100

    ejb

--- CatalogueWebEJB/ejbModule/fr/catalogue/beans/CategorieBean.java
+++ CatalogueWebEJB/ejbModule/fr/catalogue/beans/CategorieBean.java
@@ -1,2 +1,10 @@
-package fr.catalogue.beans;public class CategorieBean {
+package fr.catalogue.beans;
+
+import java.io.Serializable;
+
+public class CategorieBean implements Serializable {
+
+    private static final long serialVersionUID = 7215289928717677967L;
+
+
 }
